a705f26c96789cd1dee22c7b3d888e6d7743bb36
liankan1359 <140187454+liankan1359@users.noreply.github.com>
2024-07-11 17:28:36 +0700

    item_add_gv_update

---
+++ app/src/main/java/com/example/baitap/qlsv/Xfile.java
@@ -0,0 +1,47 @@
+package com.example.baitap.qlsv;
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class Xfile {
+    public static void ghifile(Context context, String path, Object object ){
+        try {
+            FileOutputStream fos=context.openFileOutput(path,Context.MODE_PRIVATE);
+            ObjectOutputStream oos=new ObjectOutputStream(fos);
+            oos.writeObject(object);//thực hiện ghi đối tượng
+            oos.close();
+            fos.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    //
+    public static Object docfile(Context context,String path){
+        Object list = null;
+        try{
+            FileInputStream fis=context.openFileInput(path);
+            ObjectInputStream ois=new ObjectInputStream(fis);
+            list=(ArrayList) ois.readObject();//đọc đối tượng
+            ois.close();
+            fis.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return  list;
+    }
+
+    //
+    public static boolean kiemtratontai(Context context, String fileName){
+        File file = context.getFileStreamPath(fileName);
+        return file.exists();
+    }
+}
